8d1f5396d17b11b1d9a4cce32e633d9d7a2c0699
lily <ilovesuno@proton.me>
2025-11-14 18:54:10 +0000

    test: maybe?

--- lua/cli-music/init.lua
+++ lua/cli-music/init.lua
@@ -3,6 +3,50 @@ local KeybindManager = require("core.KeybindManager")
 require("utility")
 require("dependencies")
 
-require("commands.OpenMusicPlayer")
+-- require("commands.OpenMusicPlayer")
+vim.api.nvim_create_user_command("test123", function(opts)
+    print("works")
+end, {
+    nargs = 0
+})
+
+vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
+    local Input = require("nui.input")
+    local event = require("nui.utils.autocmd").event
+
+    local input = Input({
+        position = "50%",
+        size = {
+            width = 20,
+        },
+        border = {
+            style = "single",
+            text = {
+                top = "[Howdy?]",
+                top_align = "center",
+            },
+        },
+        win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+        }, {
+            prompt = "> ",
+            default_value = "Hello",
+            on_close = function()
+                print("Input Closed!")
+            end,
+            on_submit = function(value)
+                print("Input Submitted: " .. value)
+            end,
+        })
+
+        input:mount()
+
+        input:on(event.BufLeave, function()
+        input:unmount()
+    end)
+end, {
+    nargs = 0
+})
 
 KeybindManager.load()
