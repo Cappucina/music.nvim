48ede9e794225ac226fcd3cd6ed3ba943532408e
lily <ilovesuno@proton.me>
2025-11-14 19:48:40 +0000

    .

--- lua/commands/OpenMusicPlayer.lua
+++ lua/commands/OpenMusicPlayer.lua
@@ -2,7 +2,7 @@ local function assign_command()
     local object = require("framework.components.object")
     
     vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
-        object.create_object("frame", "50%", 150, 150)
+        object.create_object("frame", "50%", 50, 50)
     end, {
         nargs = 0
     })
