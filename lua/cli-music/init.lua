d3bfc28fbaaaca70096dcd61c4738eb35d47ce21
lily <ilovesuno@proton.me>
2025-11-14 19:02:36 +0000

    .

--- lua/cli-music/init.lua
+++ lua/cli-music/init.lua
@@ -1,52 +1,10 @@
-local KeybindManager = require("core.KeybindManager")
+local cli_music = {}
 
-require("utility")
-require("dependencies")
+cli_music.setup() = function()
+    local KeybindManager = require("core.KeybindManager")
 
--- require("commands.OpenMusicPlayer")
-vim.api.nvim_create_user_command("test123", function(opts)
-    print("works")
-end, {
-    nargs = 0
-})
+    require("dependencies")
+    require("commands.OpenMusicPlayer").assign_command()
 
-vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
-    local Input = require("nui.input")
-    local event = require("nui.utils.autocmd").event
-
-    local input = Input({
-        position = "50%",
-        size = {
-            width = 20,
-        },
-        border = {
-            style = "single",
-            text = {
-                top = "[Howdy?]",
-                top_align = "center",
-            },
-        },
-        win_options = {
-            winhighlight = "Normal:Normal,FloatBorder:Normal",
-        },
-        }, {
-            prompt = "> ",
-            default_value = "Hello",
-            on_close = function()
-                print("Input Closed!")
-            end,
-            on_submit = function(value)
-                print("Input Submitted: " .. value)
-            end,
-        })
-
-        input:mount()
-
-        input:on(event.BufLeave, function()
-        input:unmount()
-    end)
-end, {
-    nargs = 0
-})
-
-KeybindManager.load()
+    KeybindManager.load()
+end
--- lua/commands/OpenMusicPlayer.lua
+++ lua/commands/OpenMusicPlayer.lua
@@ -1,44 +1,50 @@
-vim.api.nvim_create_user_command("test123", function(opts)
-    print("works")
-end, {
-    nargs = 0
-})
+local function assign_command()
+    vim.api.nvim_create_user_command("test123", function()
+        print("test")
+    end, {
+        nargs = 0
+    })
 
-vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
-    local Input = require("nui.input")
-    local event = require("nui.utils.autocmd").event
+    -- vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
+    --     local Input = require("nui.input")
+    --     local event = require("nui.utils.autocmd").event
 
-    local input = Input({
-        position = "50%",
-        size = {
-            width = 20,
-        },
-        border = {
-            style = "single",
-            text = {
-                top = "[Howdy?]",
-                top_align = "center",
-            },
-        },
-        win_options = {
-            winhighlight = "Normal:Normal,FloatBorder:Normal",
-        },
-        }, {
-            prompt = "> ",
-            default_value = "Hello",
-            on_close = function()
-                print("Input Closed!")
-            end,
-            on_submit = function(value)
-                print("Input Submitted: " .. value)
-            end,
-        })
+    --     local input = Input({
+    --         position = "50%",
+    --         size = {
+    --             width = 20,
+    --         },
+    --         border = {
+    --             style = "single",
+    --             text = {
+    --                 top = "[Howdy?]",
+    --                 top_align = "center",
+    --             },
+    --         },
+    --         win_options = {
+    --             winhighlight = "Normal:Normal,FloatBorder:Normal",
+    --         },
+    --         }, {
+    --             prompt = "> ",
+    --             default_value = "Hello",
+    --             on_close = function()
+    --                 print("Input Closed!")
+    --             end,
+    --             on_submit = function(value)
+    --                 print("Input Submitted: " .. value)
+    --             end,
+    --         })
 
-        input:mount()
+    --         input:mount()
 
-        input:on(event.BufLeave, function()
-        input:unmount()
-    end)
-end, {
-    nargs = 0
-})
+    --         input:on(event.BufLeave, function()
+    --         input:unmount()
+    --     end)
+    -- end, {
+    --     nargs = 0
+    -- })
+end
+
+return {
+    assign_command = assign_command
+}
