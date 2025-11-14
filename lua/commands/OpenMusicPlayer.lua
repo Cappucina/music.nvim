9849b97fe463a3fbe2e9d9311e5b806023102aea
lily <ilovesuno@proton.me>
2025-11-14 19:04:45 +0000

    chore: fix commands

--- lua/commands/OpenMusicPlayer.lua
+++ lua/commands/OpenMusicPlayer.lua
@@ -1,48 +1,42 @@
 local function assign_command()
-    vim.api.nvim_create_user_command("Test123", function()
-        print("test")
+    vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
+        local Input = require("nui.input")
+        local event = require("nui.utils.autocmd").event
+
+        local input = Input({
+            position = "50%",
+            size = {
+                width = 20,
+            },
+            border = {
+                style = "single",
+                text = {
+                    top = "[Howdy?]",
+                    top_align = "center",
+                },
+            },
+            win_options = {
+                winhighlight = "Normal:Normal,FloatBorder:Normal",
+            },
+            }, {
+                prompt = "> ",
+                default_value = "Hello",
+                on_close = function()
+                    print("Input Closed!")
+                end,
+                on_submit = function(value)
+                    print("Input Submitted: " .. value)
+                end,
+            })
+
+            input:mount()
+
+            input:on(event.BufLeave, function()
+            input:unmount()
+        end)
     end, {
         nargs = 0
     })
-
-    -- vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
-    --     local Input = require("nui.input")
-    --     local event = require("nui.utils.autocmd").event
-
-    --     local input = Input({
-    --         position = "50%",
-    --         size = {
-    --             width = 20,
-    --         },
-    --         border = {
-    --             style = "single",
-    --             text = {
-    --                 top = "[Howdy?]",
-    --                 top_align = "center",
-    --             },
-    --         },
-    --         win_options = {
-    --             winhighlight = "Normal:Normal,FloatBorder:Normal",
-    --         },
-    --         }, {
-    --             prompt = "> ",
-    --             default_value = "Hello",
-    --             on_close = function()
-    --                 print("Input Closed!")
-    --             end,
-    --             on_submit = function(value)
-    --                 print("Input Submitted: " .. value)
-    --             end,
-    --         })
-
-    --         input:mount()
-
-    --         input:on(event.BufLeave, function()
-    --         input:unmount()
-    --     end)
-    -- end, {
-    --     nargs = 0
-    -- })
 end
 
 return {
