19f2b35fa7fb49491c7fefb6ede01c73e6521486
lily <ilovesuno@proton.me>
2025-11-14 19:57:28 +0000

    .

--- lua/commands/OpenMusicPlayer.lua
+++ lua/commands/OpenMusicPlayer.lua
@@ -4,15 +4,21 @@ local function assign_command()
     vim.api.nvim_create_user_command("OpenMusicPlayer", function(opts)
         local background = Popup({
             enter = true,
-            border = "single" -- "double" is applicable
+            border = "single", -- "double" is applicable
+            win_options = {
+                winhighlight = "NormalFloat:BlackFloat,FloatBorder:BlackFloatBorder"
+            }
         })
+
+        vim.api.nvim_set_hl(0, "BlackFloat", { bg = "#000000", fg = "#ffffff" })
+        vim.api.nvim_set_hl(0, "BlackFloatBorder", { bg = "#000000", fg = "#ffffff" })
 
         local layout = Layout(
             {
                 position = "50%",
                 size = {
-                    width = 80,
-                    height = "60%"
+                    width = "75%",
+                    height = "80%"
                 },
             },
             Layout.Box({
